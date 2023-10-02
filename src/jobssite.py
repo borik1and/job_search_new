35a6a19c01f61284e24d01492849488493f6bf5c
Boris <borik1and@gmail.com>
2023-10-02 15:24:49 +0300

    add def superjob_search in class JodSearch

--- src/jobssite.py
+++ src/jobssite.py
@@ -24,7 +24,47 @@
 import requests
 
 
+
+
 class JodSearch:
-    def __init__(self, keyword, area):
+    def __init__(self, keyword, location):
         self.keyword = keyword
-        self.area = area
+        self.location = location
+
+    def superjob_search(self):
+        api_token = KEY_API
+        # URL для доступа к API superjob.ru
+        url = 'https://api.superjob.ru/2.0/vacancies/'
+        # Параметры запроса
+        params = {
+            'keyword': self.keyword,
+            'town': self.location,
+            'page': 1,  # Номер страницы результатов (начиная с 1)
+        }
+        # Заголовки запроса с вашим API-токеном
+        headers = {
+            'X-Api-App-Id': api_token,
+        }
+        try:
+            response = requests.get(url, params=params, headers=headers)
+            response.raise_for_status()  # Проверка на наличие ошибок
+            data = response.json()  # Преобразование ответа в формат JSON
+
+            # Вывод результатов
+            for vacancy in data['objects']:
+                print(f"Название вакансии: {vacancy['profession']}")
+
+                # Проверка наличия поля 'payment'
+                if 'payment' in vacancy:
+                    print(f"Зарплата: {vacancy['payment']}")
+                else:
+                    print("Зарплата не указана")
+
+                print(f"Город: {vacancy['town']['title']}")
+                print(f"Ссылка: {vacancy['link']}")
+                print()
+
+        except requests.exceptions.HTTPError as e:
+            print(f"Произошла ошибка при запросе к API: {e}")
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")

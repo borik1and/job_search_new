11ec77822f57831c26525eac93055cc8ebab419a
Boris <borik1and@gmail.com>
2023-10-06 12:44:09 +0300

    changing in def hh_api_search and class JobSiteAPI(ABC) in __init__

--- src/jobssite.py
+++ src/jobssite.py
@@ -1,39 +1,39 @@
 from abc import ABC, abstractmethod
+import requests
+from requests.auth import HTTPBasicAuth
+import os
 
 
 class JobSiteAPI(ABC):
-    def __init__(self, api_key='', client_id='', client_cecret=''):
-        self.api_key = api_key
-        self.client_id = client_id
-        self.client_cecret = client_cecret
+    def __init__(self):
+        self.api_token = os.getenv('API_TOKEN')
+        self.client_id = os.getenv('CLIENT_ID')
+        self.client_secret = os.getenv('CLIENT_SECRET')
 
     @abstractmethod
     def connect(self):
         pass
 
     @abstractmethod
-    def get_jobs(self, keywords, location):
+    def get_jobs(self):
         pass
 
 
-class MainJob(JobSiteAPI):
-    def __init__(self, api_key='', client_id='', client_cecret=''):
-        super().__init__(api_key, client_id, client_cecret)
-
-    pass
-
-
-import requests
-from requests.auth import HTTPBasicAuth
-
-
-class JodSearch:
+class JobSearch(JobSiteAPI):
     def __init__(self, keyword, location):
+        super().__init__()
         self.keyword = keyword
         self.location = location
 
+    def connect(self):
+        # Реализуйте настройку соединения здесь (например, получение токена доступа)
+        pass
+
+    def get_jobs(self):
+        pass
+
     def superjob_search(self):
-        api_token = KEY_API
+        api_token = self.api_token
         # URL для доступа к API superjob.ru
         url = 'https://api.superjob.ru/2.0/vacancies/'
         # Параметры запроса
@@ -50,31 +50,20 @@ class JodSearch:
             response = requests.get(url, params=params, headers=headers)
             response.raise_for_status()  # Проверка на наличие ошибок
             data = response.json()  # Преобразование ответа в формат JSON
-
-            # Вывод результатов
-            for vacancy in data['objects']:
-                print(f"Название вакансии: {vacancy['profession']}")
-
-                # Проверка наличия поля 'payment'
-                if 'payment' in vacancy:
-                    print(f"Зарплата: {vacancy['payment']}")
-                else:
-                    print("Зарплата не указана")
-
-                print(f"Город: {vacancy['town']['title']}")
-                print(f"Ссылка: {vacancy['link']}")
-                print()
+            return data  # Возвращаем результат
 
         except requests.exceptions.HTTPError as e:
             print(f"Произошла ошибка при запросе к API: {e}")
+            return None
         except Exception as e:
             print(f"Произошла ошибка: {e}")
+            return None
 
     def hh_api_search(self):
 
         # данные аутентификации
-        client_id = 'J53D8HKIPP1HV1F82IPVO5QF97NVO901RCQ72ID6UAFV6QV85FVGNL131O2NH2GI'
-        client_secret = 'H4289AFGCNFTEK9ATRTV9Q130US9VA7Q8LNE1TRQQ5GNJV0OBTBGR40SEKQOQ4IU'
+        client_id = self.client_id
+        client_secret = self.client_secret
 
         # Запрос на получение токена доступа (токена OAuth2)
         token_url = 'https://hh.ru/oauth/token'
@@ -123,3 +112,20 @@ class JodSearch:
                 print(f'Ошибка при выполнении запроса к API: {response.status_code}')
         else:
             print(f'Ошибка при получении токена доступа: {response.status_code}')
+
+
+# Создание экземпляра класса JobSearch и выполнение поиска
+job_search = JobSearch('python', 'Москва')
+superjob_data = job_search.superjob_search()
+hh_data = job_search.hh_api_search()
+
+# Теперь вы можете обработать результаты поиска, например, вывести их на экран
+if superjob_data:
+    for vacancy in superjob_data['objects']:
+        print(f"Название вакансии: {vacancy['profession']}")
+        # Остальная обработка данных SuperJob
+
+if hh_data:
+    for vacancy in hh_data['items']:
+        print(f"Название вакансии: {vacancy['name']}")
+        # Остальная обработка данных HH.ru

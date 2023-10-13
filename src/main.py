a09410be55e271261b61e652aa165b7aaa48159b
Boris <borik1and@gmail.com>
2023-10-13 17:30:20 +0300

    changing in main

--- src/main.py
+++ src/main.py
@@ -22,23 +22,23 @@ hh_vacancies = hh.format_vacancies(hh.get_vacancies())
 sj = SJ_API(keyword)
 sj_vacancies = sj.format_vacancies(sj.get_vacancies())
 
-JsonSave.all_vacancies = hh_vacancies['vacancies'] + sj_vacancies['vacancies']
-# JsonSave.add_vacancy(all_vacancies, '')
-
-
 # объединить вакансии hh и sj в один общий список с платформ
+JsonSave.all_vacancies = hh_vacancies['vacancies'] + sj_vacancies['vacancies']
 
+# сохраняем в файл json
 add_vacancy()
-sorted_vacancies = compare_vacancies_by_salary()
-pprint(sorted_vacancies)
-# Вывод отсортированных вакансий
-for vacancy in sorted_vacancies:
-    print(f"Наименование: {vacancy.name}, Зарплата: {vacancy.salary}")
 
+# sorted_vacancies = compare_vacancies_by_salary()
+# pprint(sorted_vacancies)
+# Вывод отсортированных вакансий
+# for vacancy in sorted_vacancies:
+#     print(f"Наименование: {vacancy.name}, Зарплата: {vacancy.salary}")
+
+# удалить временный файл с данными
 erase = input('Хотите удалить временный файл с данными? да или нет?: ')
 if erase == 'да' or erase == 'yes':
     if os.path.exists('vacancies.json'):
         os.remove('vacancies.json')
 else:
     pass
-# # pprint(JsonSave.all_vacancies)
+# pprint(JsonSave.all_vacancies)

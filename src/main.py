269fabb1276d575d359f5a52db55fdb1e771f25a
Boris <borik1and@gmail.com>
2023-10-14 18:42:23 +0300

    changing main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import os
 from src.class_save import JsonSave, add_vacancy
-# from pprint import pprint
 from src.class_api import HH_API, SJ_API
 from src.vacancies import Vacancy
+
+# стираем фаил json из пребедущего цикла если есть
+if os.path.exists('vacancies.json'):
+    os.remove('vacancies.json')
 
 keyword = input('Введите слово для поиска вакансий: ')
 print()
@@ -22,19 +25,17 @@ elif set_platform == '2':
     print('Ваш выбор искать на superjob.ru')
     JsonSave.all_vacancies['vacancies'] = sj_vacancies['vacancies']
 elif set_platform == '3':
-    print('Ваш выбор искать на везде')
+    print('Ваш выбор искать везде')
     JsonSave.all_vacancies['vacancies'] = hh_vacancies['vacancies'] + sj_vacancies['vacancies']
 else:
     print('Ваш выбор не опознан, ищем везде.')
     JsonSave.all_vacancies['vacancies'] = hh_vacancies['vacancies'] + sj_vacancies['vacancies']
 
-# объединить вакансии hh и sj в один общий список с платформ
-# JsonSave.all_vacancies['vacancies'] = hh_vacancies['vacancies'] + sj_vacancies['vacancies']
-
 # сохраняем в файл json
 add_vacancy()
-# Vacancy.instantiate_from_json
-
+Vacancy.compare_vacancies_by_salary()
+vac = Vacancy.print_vacancies()
+print(vac)
 # sorted_vacancies = compare_vacancies_by_salary()
 # pprint(sorted_vacancies)
 # Вывод отсортированных вакансий
@@ -43,10 +44,10 @@ add_vacancy()
 
 
 # удалить временный файл с данными
-erase = input('Хотите удалить временный файл с данными? да или нет?: ')
-if erase == 'да' or erase == 'yes':
-    if os.path.exists('vacancies.json'):
-        os.remove('vacancies.json')
-else:
-    pass
+# erase = input('Хотите удалить временный файл с данными? да или нет?: ')
+# if erase == 'да' or erase == 'yes':
+#     if os.path.exists('vacancies.json'):
+#         os.remove('vacancies.json')
+# else:
+#     pass
 # pprint(JsonSave.all_vacancies)

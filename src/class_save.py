3d80c0de608c54bc5951d3a4d2af913176af3397
Boris <borik1and@gmail.com>
2023-10-15 14:11:35 +0300

    changing in def delete_file

--- src/class_save.py
+++ src/class_save.py
@@ -1,13 +1,14 @@
 import json
+import os
 
 
 class JsonSave:
     all_vacancies = {}
 
     @staticmethod
-    def delete_file(self):
-        with open(self.file_name, 'w'):
-            pass
+    def delete_file():
+        if os.path.exists('vacancies.json'):
+            os.remove('vacancies.json')
 
     @classmethod
     def get_vacancies(self):
@@ -19,3 +20,6 @@ class JsonSave:
 def add_vacancy():
     with open('vacancies.json', 'a', encoding='utf-8') as json_file:
         json.dump(JsonSave.all_vacancies, json_file, indent=4, ensure_ascii=False)
+
+
+

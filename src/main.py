fde7f13f4e3659008b3281ae3476b7451d8f8359
Boris <borik1and@gmail.com>
2023-10-15 14:48:09 +0300

    changing in main code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-from src.class_save import JsonSave, add_vacancy
+from src.class_save import JsonSave
 from src.user_interaction import user_interaction
 
 # стираем фаил json из пребедущего цикла если есть
 JsonSave.delete_file()
 user_interaction()
-
